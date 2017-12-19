b4e5abae7d9a049a47611fdc4346731a21a69c81
ROCKET1\xudongz <xudongz@rocketsoftware.com>
2017-12-19 17:44:45 +0800

    add the dist

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+function component() {
+    var element = document.createElement('div');
+    // Lodash, now imported by this script
+    element.innerHTML = "Hi ES6.";
+    return element;
+}
+document.body.appendChild(component());
