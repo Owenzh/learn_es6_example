d22353f49655d229b451627c258bfca845ce5528
unknown <zhangxudong_sun@sina.com>
2017-12-27 22:57:34 +0800

    add clock api.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { User } from './user';
 function component() {
     var element = document.createElement('div');
     let user = new User('owen');
-    element.innerHTML = "Hi ES6. " + user.toString();
+    user.clock(element);
     return element;
 }
 
--- src/user.ts
+++ src/user.ts
@@ -7,4 +7,16 @@ export class User {
         let [a, b] = [1, 4, 5];
         return `User name is ${this.u_name} count is ${a + b}`;
     }
+    clock(ele: Element) {
+        setInterval(() => {
+            let dat = new Date();
+            let y = dat.getFullYear();
+            let m = dat.getMonth() + 1;
+            let d = dat.getDate();
+            let h = dat.getHours();
+            let MM = dat.getMinutes();
+            let s = dat.getSeconds();
+            ele.innerHTML = `当前时间 ${y}-${m}-${d} ${h}:${MM}:${s}`;
+        }, 1000)
+    }
 }
